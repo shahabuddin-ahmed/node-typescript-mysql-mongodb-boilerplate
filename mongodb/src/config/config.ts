4e698d8c7400917643f8d3d60771b1aa7de0a821
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2026-07-15 23:35:56 +0600

    updated config for mongo

--- mongodb/src/config/config.ts
+++ mongodb/src/config/config.ts
@@ -1,59 +1,33 @@
-import { Dialect } from "sequelize";
-
-interface SequelizeConfig {
-	USERNAME: string;
-	PASSWORD: string;
-	MYSQL_DATABASE: string;
+interface MongConfig {
+    MONGO_HOST: string;
+    MONGO_DB: string;
 }
 
-interface SequelizeOptions {
-	host: string;
-	dialect: Dialect;
-	pool: {
-		max: number;
-		min: number;
-		idle: number;
-	};
-	define: {
-		timestamps: boolean
-	},
-	charset: string;
-	collate: string;
-	logging: boolean;
-	log: any;
+interface SocketConfig {
+    SOCKET_CORS: string[];
 }
 
 interface Config {
-	SEQUELIZE: SequelizeConfig;
-	SEQUELIZEOPTIONS: SequelizeOptions;
-	APPLICATION_SERVER_PORT: number;
-	APP_FORCE_SHUTDOWN_SECOND: number;
+    MONGO: MongConfig;
+    SOCKET: SocketConfig;
+    APPLICATION_SERVER_PORT: number;
+    APP_FORCE_SHUTDOWN_SECOND: number;
 }
 
 const config: Config = {
-	SEQUELIZE: {
-		USERNAME: process.env.MYSQL_USER || "root",
-		PASSWORD: process.env.MYSQL_PASSWORD || "root",
-		MYSQL_DATABASE: process.env.MYSQL_DATABASE || "testdb"
-	},
-	SEQUELIZEOPTIONS: {
-		host: process.env.MYSQL_HOST || "0.0.0.0",
-		dialect: "mysql",
-		pool: {
-			max: 20,
-			min: 5,
-			idle: 10000
-		},
-		define: {
-			timestamps: false
-		},
-		charset: "utf8",
-		collate: "utf8_general_ci",
-		logging: false,
-		log: console.log
-	},
-	APPLICATION_SERVER_PORT: Number(process.env.APPLICATION_SERVER_PORT) || 3000,
-	APP_FORCE_SHUTDOWN_SECOND: Number(process.env.APP_FORCE_SHUTDOWN_SECOND) || 30
+    MONGO: {
+        MONGO_HOST: process.env.MONGO_HOST || "mongodb://127.0.0.1:27017",
+        MONGO_DB: process.env.MONGO_DB || "sample-app",
+    },
+    SOCKET: {
+        SOCKET_CORS: process.env.SOCKET_CORS
+            ? process.env.SOCKET_CORS.split(",")
+            : ["http://localhost:3001"],
+    },
+    APPLICATION_SERVER_PORT:
+        Number(process.env.APPLICATION_SERVER_PORT) || 3000,
+    APP_FORCE_SHUTDOWN_SECOND:
+        Number(process.env.APP_FORCE_SHUTDOWN_SECOND) || 30,
 };
 
 export default config;
